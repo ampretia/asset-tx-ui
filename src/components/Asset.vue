394d9a5e97815df0fb089d20a244b140dbd4a8a3
Matthew B White <github@mh-white.com>
2022-07-16 09:43:51 +0100

    First commit
    Signed-off-by: Matthew B White <github@mh-white.com>

---
+++ src/components/Asset.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Asset",
+  props: {
+    owner: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
